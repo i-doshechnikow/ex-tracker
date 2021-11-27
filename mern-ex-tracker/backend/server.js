f2ff50a55a87e94b39cab846948ae00dcf3f0c0f
iliya <iliya.yurievich@yandex.ru>
2021-11-27 11:09:26 +0300

    edit git ignore and add connect to mongo

--- mern-ex-tracker/backend/server.js
+++ mern-ex-tracker/backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -8,11 +8,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
-// its just for test
-// app.get("/", async (req, res) => {
-//   res.send("hello");
-// });
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("mongo connection success");
+});
 
 app.listen(port, () => {
   console.log("Server run on port 5000");
